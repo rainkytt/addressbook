3f4cd3ec96fbbb1a7954aefe3b0692ac10c0af7a
Rain <rain.kutt@khk.ee>
2018-12-07 12:04:21 +0200

    The Delete Button

--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -97,4 +97,13 @@ public class PersonOverviewController {
         }
     }
 
+    /**
+     * Called when the user clicks on the delete button.
+     */
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        personTable.getItems().remove(selectedIndex);
+    }
+
 }
