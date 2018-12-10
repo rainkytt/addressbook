b4a997f21b1ebb36be5670f4a762f84cc60ff99d
Rain <rain.kutt@khk.ee>
2018-12-10 17:36:32 +0200

    Adding an Application Icon

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.Modality;
+import javafx.scene.image.Image;
 
 public class MainApp extends Application {
 
@@ -55,6 +56,9 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+
+        // Set the application icon.
+        this.primaryStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
 
         initRootLayout();
 
